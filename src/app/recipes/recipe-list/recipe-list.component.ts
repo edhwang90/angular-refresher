b99c9c7e5fae6fa3fb8f920d94cb1d1e163a4e94
Edy Hwang <42156235+edhwang90@users.noreply.github.com>
2022-10-18 11:00:21 -0400

    databinding
    databinding initial example methods

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe("A Test Recipe", "Simply a test", "https://th.bing.com/th/id/OIP.YixleAz2dzjXRd3FGfaIlwHaE7?pid=ImgDet&rs=1"),
     new Recipe("A Second Recipe", "Simply a test", "https://th.bing.com/th/id/R.228f96159f5cc6b1c3d1b548eb64644c?rik=YxtwFGdj%2fyo%2ffA&riu=http%3a%2f%2fchewtheworld.com%2fwp-content%2fuploads%2f2016%2f05%2falfredo-sauce-1.jpg&ehk=1p7wjBjGr1EAFsLNIJ2k%2fd8NebLkaSYnVqPSx42baEI%3d&risl=&pid=ImgRaw&r=0")
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }

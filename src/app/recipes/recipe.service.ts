864fe17d425ed8f602ec64f8d71901497660fa39
Edy Hwang <42156235+edhwang90@users.noreply.github.com>
2022-10-19 11:43:16 -0400

    routing
    routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -29,7 +29,11 @@ export class RecipeService {
   constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
-    return this.recipes.slice();
+    return this.recipes.slice(); // return new exact copy
+  }
+
+  getRecipe(index: number) {
+    return this.recipes.slice()[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
